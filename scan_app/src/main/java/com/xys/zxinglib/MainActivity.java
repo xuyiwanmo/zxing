6c72b156b8d1e1e9e1740975d7ad805a76534d29
zhanghua <582605786@qq.com>
2016-09-07 16:17:14 +0800

    1.扫描精简

---
+++ scan_app/src/main/java/com/xys/zxinglib/MainActivity.java
@@ -0,0 +1,56 @@
+package com.xys.zxinglib;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.xys.libzxing.zxing.activity.CaptureActivity;
+
+public class MainActivity extends Activity {
+
+    private TextView resultTextView;
+    private EditText qrStrEditText;
+    private ImageView qrImgImageView;
+    private CheckBox mCheckBox;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        resultTextView = (TextView) this.findViewById(R.id.tv_scan_result);
+        qrStrEditText = (EditText) this.findViewById(R.id.et_qr_string);
+        qrImgImageView = (ImageView) this.findViewById(R.id.iv_qr_image);
+        mCheckBox = (CheckBox) findViewById(R.id.logo);
+
+        Button scanBarCodeButton = (Button) this.findViewById(R.id.btn_scan_barcode);
+        scanBarCodeButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //打开扫描界面扫描条形码或二维码
+                Intent openCameraIntent = new Intent(MainActivity.this, CaptureActivity.class);
+                startActivityForResult(openCameraIntent, 0);
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            Bundle bundle = data.getExtras();
+            String scanResult = bundle.getString("result");
+            resultTextView.setText(scanResult);
+        }
+    }
+}
